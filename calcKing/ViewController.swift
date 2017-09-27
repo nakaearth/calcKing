3c191b83d9e62d43a1be0806c05d1f2aebaf8058
nakaearth <naka5313@gmail.com>
2017-09-27 22:48:25 +0900

    Initial Commit

---
+++ calcKing/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  calcKing
+//
+//  Created by 真一郎中村 on 2017/09/27.
+//  Copyright © 2017年 真一郎中村. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
