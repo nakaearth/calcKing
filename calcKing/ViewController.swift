ab58729e8bf4efb0c96cb4c70a0f6bb9301bb0ec
nakaearth <naka5313@gmail.com>
2017-09-29 06:00:49 +0900

    電卓のサンプル途中

--- calcKing/ViewController.swift
+++ calcKing/ViewController.swift
@@ -9,17 +9,43 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var formulaLabel: UILabel!
+    @IBOutlet weak var answerLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // ビューがロードされた時点で式と答えのラベルは空
+        formulaLabel.text = ""
+        answerLabel.text  = ""
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    @IBAction func inputFormula(_ sender: UIButton) {
+        // ボタン（Cと=以外）が押されたら式を表示する
+        guard let formulaText = formulaLabel.text else {
+            return
+        }
+        guard let senderedText = sender.titleLabel?.text else {
+            return
+        }
+        formulaLabel.text = formulaText + senderedText
+    }
+    
+    @IBAction func clearCalculation(_ sender: UIButton) {
+        // Cボタンが押されたら式と答えをクリアする
+        formulaLabel.text = ""
+        answerLabel.text = ""
+    }
+    
+    @IBAction func calculateAnswer(_ sender: UIButton) {
+        // =ボタンが押されたら答えを計算して表示する
+        guard let formulaText = formulaLabel.text else {
+            return
+        }
+        answerLabel.text = formulaText
+    }
 }
 
